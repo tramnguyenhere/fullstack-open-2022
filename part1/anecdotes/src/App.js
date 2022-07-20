b9e2d66ee65d92bb6cebeaa2178b13ed939489e9
Tram Nguyen <tramngoc1402@gmail.com>
2022-07-20 22:13:27 +0300

    Finished part 1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,18 +10,37 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
   ];
-
   const [selected, setSelected] = useState(0);
-  const handleClick = () => {
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length));
+  const [maxVoteIndex, setMaxVoteIndex] = useState();
+
+  const handleQuote = () => {
     const newSelect = Math.floor(Math.random() * anecdotes.length);
     setSelected(newSelect);
   };
 
+  const handleVotes = () => {
+    const newVotes = [...votes]; //copy old array into a new one
+    newVotes[selected]++;
+    setVotes(newVotes);
+
+    //Find max vote index
+    const max = newVotes.indexOf(Math.max(...newVotes));
+    setMaxVoteIndex(max);
+  };
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <br></br>
-      <button onClick={handleClick}>next anecdote</button>
+      <p>has {votes[selected]} votes</p>
+      <br></br>
+      <button onClick={handleVotes}>vote</button>
+      <button onClick={handleQuote}>next anecdote</button>
+      <h1>Anecdote with the most votes</h1>
+      {anecdotes[maxVoteIndex]}
+      <p>has {votes[maxVoteIndex]} votes</p>
     </div>
   );
 };
