2e6fe51a2a045e1bc94c5df2ba9fd093c81e6390
Tram Nguyen <tramngoc1402@gmail.com>
2022-08-31 22:52:16 +0300

    2.4 done

--- part2/courseinfo-2/src/components/Course.js
+++ part2/courseinfo-2/src/components/Course.js
@@ -15,14 +15,15 @@ const Total = ({ parts }) => {
   const total = parts.reduce((acc, part) => acc + part.exercises, 0);
   return <h3>total of {total} exercises</h3>;
 };
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
-
+const Course = ({ courses }) => (
+  <div>
+    {courses.map((course) => (
+      <div key={course.id}>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    ))}
+  </div>
+);
 export default Course;
