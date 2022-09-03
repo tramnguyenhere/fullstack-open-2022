9569f31d322c5a66d01efd9b354c27e37cab815b
Tram Nguyen <tramngoc1402@gmail.com>
2022-09-03 18:51:05 +0300

    2.14 Done

--- part2/country-data/src/components/Country.js
+++ part2/country-data/src/components/Country.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Country = ({ countries }) => {
+  const [weather, setWeather] = useState([]);
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${countries.capital}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      });
+  }, [countries]);
   return (
     <div>
       <h1>{countries.name.common}</h1>
@@ -13,6 +25,15 @@ const Country = ({ countries }) => {
         ))}
       </ul>
       <img src={countries.flags.png} alt={countries.name.common}></img>
+      <h2>Weather in {countries.capital}</h2>
+      <p>temperature {weather.main && weather.main.temp} Celcius</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${
+          weather.weather && weather.weather[0].icon
+        }@2x.png`}
+        alt={countries.capital}
+      />
+      <p>wind {weather.wind && weather.wind.speed} m/s</p>
     </div>
   );
 };
