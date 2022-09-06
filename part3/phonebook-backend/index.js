a62a4fe7b99e60eee21c4b92c7c0e34256dab71e
Tram Nguyen <tramngoc1402@gmail.com>
2022-09-06 17:48:10 +0300

    3.5 and 3.6 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,6 +2,8 @@ const { request } = require('express');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -35,6 +37,48 @@ app.get('/info', (request, response) => {
         <p>${new Date().toString()}</p>
     </div>
  `);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id == id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000000);
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({ error: 'name must be missing' });
+  } else if (!body.number) {
+    return response.status(400).json({ error: 'number must be missing' });
+  } else if (persons.filter((person) => person.name == body.name)) {
+    return response.status(400).json({ error: 'name must be unique' });
+  } else {
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number,
+    };
+
+    persons = persons.concat(person);
+    response.json(person);
+  }
 });
 
 const PORT = 3001;
