69d7096fef9aa17aede1d3193ddb3823d13e55dd
Tram Nguyen <tramngoc1402@gmail.com>
2022-10-05 18:47:17 +0300

    Part 6 Done

--- part6/anecdotes-redux/src/index.js
+++ part6/anecdotes-redux/src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { configureStore } from '@reduxjs/toolkit';
+
 import { Provider } from 'react-redux';
 import App from './App';
-import anecdoteReducer from './reducers/anecdoteReducer';
-import notificationReducer from './reducers/notificationReducer';
-import filterReducer from './reducers/filterReducer';
 
-const store = configureStore({
-  reducer: {
-    anecdotes: anecdoteReducer,
-    notification: notificationReducer,
-    filter: filterReducer,
-  },
-});
-
-console.log(store.getState());
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
