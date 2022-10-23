f4cf80fa33857d6661f617154633a0ced2271bf6
Tram Nguyen <tramngoc1402@gmail.com>
2022-10-23 16:12:44 +0300

    initialize on first prj

---
+++ part9/starter-project/bmiCalculator.ts
@@ -0,0 +1,51 @@
+interface BodyValues {
+    height: number,
+    weight: number
+}
+
+const parseArguments = (args: Array<string>): BodyValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not number!');
+    }
+}
+
+const calculateBmi = (a: number, b: number) => {
+    const bmi = a / (b * b);
+    if (bmi < 16.0) {
+        console.log('Underweight (Severe thinness)');
+    } else if (bmi > 16.0 && bmi < 16.9) {
+        console.log('Underweight (Moderate thinness)');
+    } else if (bmi > 17.0 && bmi < 18.4) {
+        console.log('Underweight (Mild thinness)');
+    } else if (bmi > 18.5 && bmi < 24.9) {
+        console.log('Normal range');
+    } else if (bmi > 25 && bmi < 29.9) {
+        console.log('Overweight (Pre-obese)');
+    } else if (bmi > 30 && bmi < 34.9) {
+        console.log('Obese (Class I)');
+    } else if (bmi > 35 && bmi < 39.9) {
+        console.log('Obese (Class II)');
+    } else if (bmi >= 40.0) {
+        console.log('Obese (Class III)');
+    } 
+
+}
+
+try {
+    const {weight, height} = parseArguments(process.argv);
+    calculateBmi(weight, height);
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += 'Error' + error.message;
+    }
+    console.log(errorMessage); 
+}
---
+++ part9/starter-project/exerciseCalculator.ts
@@ -0,0 +1,77 @@
+interface ExerciseData {
+    target: number;
+    dailyExerciseData: Array<number>
+}
+
+const parseExerciseArgument = (args: Array<string>): ExerciseData => {
+    if (args.length < 3) throw new Error('Not enough arguments');
+    let check = true;
+    for (let i = 3; i < args.length - 2; i++) {
+        if (isNaN(Number(args[i]))) {
+            check = false;
+        }
+    }
+    if (!isNaN(Number(args[2])) && check) {
+        let data = [];
+        for (let i = 3; i < args.length - 2; i++) {
+            data.push(Number(args[i]))
+        }
+        return { 
+            target: Number(args[2]),
+            dailyExerciseData: data 
+        }
+    } else {
+        throw new Error('Provided values were not number!');
+    }
+}
+
+interface Result {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number
+}
+
+const exerciseCalculator = (target: number, dailyExerciseData: Array<number>): Result => {
+    const periodLength = dailyExerciseData.length;
+    const trainingDays = dailyExerciseData.filter(d => d > 0).length;
+    const average = dailyExerciseData.reduce((sum, a) => sum + a, 0) / periodLength;
+    const success = average >= target ? true : false;
+    let rating;
+    let ratingDescription;
+
+    if (average < target) {
+        rating = 1;
+        ratingDescription = 'not too bad but could be better';
+    } else if (average === target) {
+        rating = 2;
+        ratingDescription='well done'
+    } else {
+        rating = 3;
+        ratingDescription='bravooo!'
+    }
+    
+    return {
+        periodLength: periodLength,
+        trainingDays: trainingDays,
+        success: success,
+        rating: rating,
+        ratingDescription: ratingDescription,
+        target: target,
+        average: average
+    }
+}
+
+try {
+    const {target, dailyExerciseData} = process.argv;
+    exerciseCalculator(target, dailyExerciseData);
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += 'Error' + error.message;
+    }
+    console.log(errorMessage); 
+}
